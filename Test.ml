f6e862bec0f8e740d11225efef83feb66797727c
William Henry Slocum <william.slocum@uvm.edu>
2018-05-03 23:29:24 -0400

    Overhauled Testing Suite

--- Test.ml
+++ Test.ml
@@ -10,318 +10,54 @@ open Final
 
 (* ====================================================================== *)
 
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
+let tests : (exp) list =
+  [If(IsZero(Zero),Pred(Succ(Zero)),Succ(Zero));
+   IsZero(Apply(Lambda("X0", Var("X0")),Succ(Zero)));
+   Apply(Lambda("x",Var("x")),Lambda("y", Var("y")));
+   If(IsZero(Zero),Pred(Succ(Zero)),Succ(Zero));
+   Lambda("x",Succ(Var("x")));
+   Lambda("x",Succ(Var("x")));
+   If(Pred(Zero),True,False);
+   Lambda("x",If(Var("x"),False,False));
+   Lambda("x",If(Var("x"),False,IsZero(False)));
+   Apply(Apply(Lambda("w",Var("w")),Lambda("x",Var("x"))),Apply(Lambda("y",Var("y")),Lambda("z",Var("z"))));
+   LambdaA("x",Bool,If(Var("x"),Var("x"),Var("x")));
+   Lambda("x",IsZero(If(Var("x"),Zero,Pred(Zero))));
+   If(True,Lambda("x",Var("x")),Lambda("x",Var("x")));
+  ] ;;
+
+List.iter (fun (e) ->
+    print_string " " ;
+    print_endline " " ; print_string "--" ; print_endline " " ;
+
+    print_endline " " ; print_string "EXPRESSION" ; print_endline " " ;
+    print_endline ([%show : exp] e) ;
+
+    let v = infer (StringMap.empty) e (TermPairSet.empty) in
+
+    match v with
+    | Val(t,c) ->
+      print_endline " " ; print_string "CONSTRAINT SET" ; print_endline " " ;
+      print_endline ([%show : constr] c);
+      let u = unify c (TermPairSet.empty) in
+      begin match u with
+        | Val(c1,c2) ->
+          print_endline " " ; print_string "SOLUTION" ; print_endline " " ;
+          print_endline ([%show : constr] c2) ;
+          print_endline " " ; print_string "TYPE" ; print_endline " " ;
+          print_endline ([%show : ty] t)
+        | _ ->
+          print_endline " " ; print_string "SOLUTION" ; print_endline " " ;
+          print_endline "CANNOT BE UNIFIED" ;
+          print_endline " " ; print_string "TYPE" ; print_endline " " ;
+          print_endline "UNTYPEABLE" ;
+      end
+    | _ ->
+      print_endline " " ; print_string "CONSTRAINT SET" ; print_endline " " ;
+      print_endline "CONSTRAINT SET ERROR" ;
+
+
+  ) tests
 
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = IsZero(Apply(LambdaA("x",Nat,Var("x")),Zero)) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = If(IsZero(Zero),Pred(Succ(Zero)),Succ(Zero)) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = IsZero(Apply(Lambda("X0", Var("X0")),Succ(Zero))) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = Apply(Lambda("x",Var("x")),Lambda("y", Var("y"))) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = If(IsZero(Zero),Pred(Succ(Zero)),Succ(Zero)) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = Lambda("x",Succ(Var("x"))) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = Lambda("x",Succ(Var("x"))) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = If(Pred(Zero),True,False)
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = Lambda("x",If(Var("x"),False,False)) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = Lambda("x",If(Var("x"),False,IsZero(False))) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = Apply(Apply(Lambda("w",Var("w")),Lambda("x",Var("x"))),Apply(Lambda("y",Var("y")),Lambda("z",Var("z")))) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = LambdaA("x",Bool,If(Var("x"),Var("x"),Var("x"))) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_string " " ;;
-print_endline " " ;; print_string "--" ;; print_endline " " ;;
-
-print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
-let e = Lambda("x",IsZero(If(Var("x"),Zero,Pred(Zero)))) ;;
-let _ = print_endline ([%show : exp] e) ;;
-
-print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
-let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
-print_endline ([%show : result] v);;
-
-print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
-match v with
-| Val(t,c) ->
-  let u = unify c (TermPairSet.empty) in
-  begin match u with
-    | Val(c1,c2) ->
-      print_endline ([%show : constr] c2)
-    | _ -> print_endline "STUCK"
-  end
-| _ -> print_endline "STUCK"
-
-(* ====================================================================== *)
-
-let _ = print_endline " " ;;
 
 (* Name: <William H Slocum> *)
