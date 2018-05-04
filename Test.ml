b49337f0373077015e7d3f0dae4a6492d6d881aa
William Henry Slocum <william.slocum@uvm.edu>
2018-05-03 20:54:34 -0400

    Testing...

--- Test.ml
+++ Test.ml
@@ -11,7 +11,7 @@ print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
 let e = IsZero(Apply(LambdaA("x",Nat,Var("x")),Zero)) ;;
 let _ = print_endline ([%show : exp] e) ;;
 
-print_endline " " ;; print_string "CONSTRAINT SET" ;; print_endline " " ;;
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
 let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
 print_endline ([%show : result] v);;
 
@@ -35,7 +35,7 @@ print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
 let e = If(IsZero(Zero),Pred(Succ(Zero)),Succ(Zero)) ;;
 let _ = print_endline ([%show : exp] e) ;;
 
-print_endline " " ;; print_string "CONSTRAINT SET" ;; print_endline " " ;;
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
 let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
 print_endline ([%show : result] v);;
 
@@ -59,7 +59,7 @@ print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
 let e = IsZero(Apply(Lambda("X0", Var("X0")),Succ(Zero))) ;;
 let _ = print_endline ([%show : exp] e) ;;
 
-print_endline " " ;; print_string "CONSTRAINT SET" ;; print_endline " " ;;
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
 let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
 print_endline ([%show : result] v);;
 
@@ -83,7 +83,7 @@ print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
 let e = Apply(Lambda("x",Var("x")),Lambda("y", Var("y"))) ;;
 let _ = print_endline ([%show : exp] e) ;;
 
-print_endline " " ;; print_string "CONSTRAINT SET" ;; print_endline " " ;;
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
 let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
 print_endline ([%show : result] v);;
 
@@ -107,7 +107,7 @@ print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
 let e = If(IsZero(Zero),Pred(Succ(Zero)),Succ(Zero)) ;;
 let _ = print_endline ([%show : exp] e) ;;
 
-print_endline " " ;; print_string "CONSTRAINT SET" ;; print_endline " " ;;
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
 let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
 print_endline ([%show : result] v);;
 
@@ -131,7 +131,7 @@ print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
 let e = Lambda("x",Succ(Var("x"))) ;;
 let _ = print_endline ([%show : exp] e) ;;
 
-print_endline " " ;; print_string "CONSTRAINT SET" ;; print_endline " " ;;
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
 let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
 print_endline ([%show : result] v);;
 
@@ -155,7 +155,7 @@ print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
 let e = Lambda("x",Succ(Var("x"))) ;;
 let _ = print_endline ([%show : exp] e) ;;
 
-print_endline " " ;; print_string "CONSTRAINT SET" ;; print_endline " " ;;
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
 let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
 print_endline ([%show : result] v);;
 
@@ -171,3 +171,30 @@ match v with
 | _ -> print_string "STUCK"
 
 (* ====================================================================== *)
+
+let _ = print_string " " ;;
+print_endline " " ;; print_string "--" ;; print_endline " " ;;
+
+print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
+let e = If(Pred(Zero),True,False)
+let _ = print_endline ([%show : exp] e) ;;
+
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
+let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
+print_endline ([%show : result] v);;
+
+print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
+match v with
+| Val(t,c) ->
+  let u = unify c (TermPairSet.empty) in
+  begin match u with
+    | Val(c1,c2) ->
+      print_endline ([%show : constr] c2)
+    | _ -> print_string "STUCK"
+  end
+| _ -> print_string "STUCK"
+
+(* ====================================================================== *)
+
+let _ = print_string " " ;;
+print_endline " " ;; print_string "--" ;; print_endline " " ;;
