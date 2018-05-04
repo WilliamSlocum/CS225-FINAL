6621ca3f1a05b91a47dfafbf97f78d19bff77cf8
William Henry Slocum <william.slocum@uvm.edu>
2018-05-03 20:25:15 -0400

    Making Test Output Look Nice

--- Final.ml
+++ Final.ml
@@ -331,12 +331,26 @@ let rec unify (c : constr) (sb : constr) : uresult =
       end
     end
 
-let _ = print_endline ([%show : string] "---------------------------------") ;;
+let _ = print_string "--" ;;
 
+print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
 let e = IsZero(Apply(LambdaA("x",Nat,Var("x")),Zero)) ;;
-let c = infer (StringMap.empty) e (TermPairSet.empty) ;;
-let _ = print_endline ([%show : result] c) ;;
-match c with | Val(t,c) -> let u = unify c (TermPairSet.empty) in print_endline ([%show : uresult] u)
+let _ = print_endline ([%show : exp] e) ;;
+
+print_endline " " ;; print_string "CONSTRAINT SET" ;; print_endline " " ;;
+let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
+print_endline ([%show : result] v);;
+
+print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
+match v with
+| Val(t,c) ->
+  let u = unify c (TermPairSet.empty) in
+  begin match u with
+    | Val(c1,c2) ->
+      let u = unify c (TermPairSet.empty) in print_endline ([%show : constr] c2)
+    | _ -> print_string "STUCK"
+  end
+| _ -> print_string "STUCK"
 
 let _ = print_endline ([%show : string] "---------------------------------") ;;
 
