4eaab8992370e4a51d0875625c9bee35c3f94320
William Henry Slocum <william.slocum@uvm.edu>
2018-05-03 21:17:22 -0400

    Testing...

--- Test.ml
+++ Test.ml
@@ -198,3 +198,52 @@ match v with
 
 let _ = print_string " " ;;
 print_endline " " ;; print_string "--" ;; print_endline " " ;;
+
+print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
+let e = Lambda("x",If(Var("x"),False,False)) ;;
+let _ = print_endline ([%show : exp] e) ;;
+
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
+let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
+print_endline ([%show : result] v);;
+
+print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
+match v with
+| Val(t,c) ->
+  let u = unify c (TermPairSet.empty) in
+  begin match u with
+    | Val(c1,c2) ->
+      print_endline ([%show : constr] c2)
+    | _ -> print_string "STUCK"
+  end
+| _ -> print_string "STUCK"
+
+(* ====================================================================== *)
+
+let _ = print_string " " ;;
+print_endline " " ;; print_string "--" ;; print_endline " " ;;
+
+
+print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
+let e = Lambda("x",If(Var("x"),False,IsZero(False))) ;;
+let _ = print_endline ([%show : exp] e) ;;
+
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
+let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
+print_endline ([%show : result] v);;
+
+print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
+match v with
+| Val(t,c) ->
+  let u = unify c (TermPairSet.empty) in
+  begin match u with
+    | Val(c1,c2) ->
+      print_endline ([%show : constr] c2)
+    | _ -> print_string "STUCK"
+  end
+| _ -> print_string "STUCK"
+
+(* ====================================================================== *)
+
+let _ = print_string " " ;;
+print_endline " " ;; print_string "--" ;; print_endline " " ;;
