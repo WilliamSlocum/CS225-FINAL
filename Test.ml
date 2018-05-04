5437dc7b52f35eb077ba663881b901460b62fafc
William Henry Slocum <william.slocum@uvm.edu>
2018-05-03 21:47:43 -0400

    Testing..

--- Test.ml
+++ Test.ml
@@ -223,9 +223,80 @@ match v with
 let _ = print_string " " ;;
 print_endline " " ;; print_string "--" ;; print_endline " " ;;
 
-
 print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
 let e = Lambda("x",If(Var("x"),False,IsZero(False))) ;;
+let _ = print_endline ([%show : exp] e) ;;
+
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
+let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
+print_endline ([%show : result] v);;
+
+print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
+match v with
+| Val(t,c) ->
+  let u = unify c (TermPairSet.empty) in
+  begin match u with
+    | Val(c1,c2) ->
+      print_endline ([%show : constr] c2)
+    | _ -> print_string "STUCK"
+  end
+| _ -> print_string "STUCK"
+
+(* ====================================================================== *)
+
+let _ = print_string " " ;;
+print_endline " " ;; print_string "--" ;; print_endline " " ;;
+
+print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
+let e = Apply(Apply(Lambda("w",Var("w")),Lambda("x",Var("x"))),Apply(Lambda("y",Var("y")),Lambda("z",Var("z")))) ;;
+let _ = print_endline ([%show : exp] e) ;;
+
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
+let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
+print_endline ([%show : result] v);;
+
+print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
+match v with
+| Val(t,c) ->
+  let u = unify c (TermPairSet.empty) in
+  begin match u with
+    | Val(c1,c2) ->
+      print_endline ([%show : constr] c2)
+    | _ -> print_string "STUCK"
+  end
+| _ -> print_string "STUCK"
+
+(* ====================================================================== *)
+
+let _ = print_string " " ;;
+print_endline " " ;; print_string "--" ;; print_endline " " ;;
+
+print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
+let e = LambdaA("x",Bool,If(Var("x"),Var("x"),Var("x"))) ;;
+let _ = print_endline ([%show : exp] e) ;;
+
+print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
+let v = infer (StringMap.empty) e (TermPairSet.empty) ;;
+print_endline ([%show : result] v);;
+
+print_endline " " ;; print_string "SOLUTION" ;; print_endline " " ;;
+match v with
+| Val(t,c) ->
+  let u = unify c (TermPairSet.empty) in
+  begin match u with
+    | Val(c1,c2) ->
+      print_endline ([%show : constr] c2)
+    | _ -> print_string "STUCK"
+  end
+| _ -> print_string "STUCK"
+
+(* ====================================================================== *)
+
+let _ = print_string " " ;;
+print_endline " " ;; print_string "--" ;; print_endline " " ;;
+
+print_endline " " ;; print_string "EXPRESSION" ;; print_endline " " ;;
+let e = Lambda("x",IsZero(If(Var("x"),Zero,Pred(Zero)))) ;;
 let _ = print_endline ([%show : exp] e) ;;
 
 print_endline " " ;; print_string "TYPE * CONSTRAINT SET" ;; print_endline " " ;;
