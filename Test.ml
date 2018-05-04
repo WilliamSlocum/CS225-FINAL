0df479da6cec2c5e228751a37f0f2c4ed1e0190a
William Henry Slocum <william.slocum@uvm.edu>
2018-05-04 00:14:32 -0400

    Function to Calculate Expression Type Based on Unification Solution

--- Test.ml
+++ Test.ml
@@ -10,20 +10,40 @@ open Final
 
 (* ====================================================================== *)
 
+(* Function to Perform Substitution on a Type Given a Unification Solution *)
+let rec solvetype (c : constr) (t : ty) : ty =
+  if TermPairSet.is_empty c
+  then t
+  else
+    let el = TermPairSet.choose c in
+    let c' = TermPairSet.remove el c in
+    begin match el with
+      | (xT,tS) ->
+        begin match xT with
+          | TVar(xt) ->
+            let t' = tsubst xt t tS in
+            solvetype c' t'
+          | _ -> solvetype c' t
+        end
+    end
+
 let tests : (exp) list =
-  [If(IsZero(Zero),Pred(Succ(Zero)),Succ(Zero));
-   IsZero(Apply(Lambda("X0", Var("X0")),Succ(Zero)));
-   Apply(Lambda("x",Var("x")),Lambda("y", Var("y")));
-   If(IsZero(Zero),Pred(Succ(Zero)),Succ(Zero));
+  [If(Zero,True,False);
+   If(IsZero(Zero),Pred(Zero),Succ(Zero));
+   If(True,Lambda("x",Var("x")),Lambda("y",Var("y")));
+   Succ(Lambda("x",Succ(Var("x"))));
+   Pred(Lambda("x",Succ(Var("x"))));
+   IsZero(Apply(Lambda("x", Var("x")),Succ(Zero)));
    Lambda("x",Succ(Var("x")));
-   Lambda("x",Succ(Var("x")));
-   If(Pred(Zero),True,False);
-   Lambda("x",If(Var("x"),False,False));
-   Lambda("x",If(Var("x"),False,IsZero(False)));
-   Apply(Apply(Lambda("w",Var("w")),Lambda("x",Var("x"))),Apply(Lambda("y",Var("y")),Lambda("z",Var("z"))));
-   LambdaA("x",Bool,If(Var("x"),Var("x"),Var("x")));
+   Lambda("x",If(Var("x"),True,Var("x")));
    Lambda("x",IsZero(If(Var("x"),Zero,Pred(Zero))));
-   If(True,Lambda("x",Var("x")),Lambda("x",Var("x")));
+   Lambda("x",Lambda("y",IsZero(If(Var("x"),Zero,Pred(Var("y"))))));
+   LambdaA("x",Bool,If(Var("x"),Var("x"),Var("x")));
+   LambdaA("x",Fun(Nat,Bool),Apply(Var("x"),Zero));
+   LambdaA("x",Bool,Lambda("y",If(Var("x"),Var("y"),Zero)));
+   LambdaA("x",Bool,Lambda("y",If(Var("y"),Var("x"),Zero)));
+   Apply(Lambda("x",Var("x")),Lambda("y", Var("y")));
+   Apply(Apply(Lambda("w",Var("w")),Lambda("x",Var("x"))),Apply(Lambda("y",Var("y")),Lambda("z",Var("z"))));
   ] ;;
 
 List.iter (fun (e) ->
@@ -45,7 +65,8 @@ List.iter (fun (e) ->
           print_endline " " ; print_string "SOLUTION" ; print_endline " " ;
           print_endline ([%show : constr] c2) ;
           print_endline " " ; print_string "TYPE" ; print_endline " " ;
-          print_endline ([%show : ty] t)
+          let t' = solvetype c t in
+          print_endline ([%show : ty] t')
         | _ ->
           print_endline " " ; print_string "SOLUTION" ; print_endline " " ;
           print_endline "CANNOT BE UNIFIED" ;
